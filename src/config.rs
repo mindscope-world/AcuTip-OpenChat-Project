320de91d7a264f4a3f33d093a8fe6070db9f1262
mindscope-world <ndirangup254@gmail.com>
2025-04-30 18:50:58 +0300

    Docker commit.

--- src/config.rs
+++ src/config.rs
@@ -1,15 +1,24 @@
 use serde::Deserialize;
 use std::fs;
 use tracing::Level;
+use std::env;
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct Config {
     pub pem_file: String,
     pub ic_url: String,
     pub oc_public_key: String,
+    #[serde(default = "default_port")]
     pub port: u16,
     #[serde(with = "LevelDef")]
     pub log_level: Level,
+}
+
+fn default_port() -> u16 {
+    env::var("PORT")
+        .ok()
+        .and_then(|p| p.parse().ok())
+        .unwrap_or(13457)
 }
 
 #[derive(Deserialize)]
